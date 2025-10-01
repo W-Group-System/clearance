35e21f8e96b59754f2937c32395ce9e90c83efe5
Schultz Henry Boado <schultzxhenry@gmail.com>
2025-10-01 11:29:03 +0800

    add soft delete

--- app/ExitResign.php
+++ app/ExitResign.php
@@ -3,10 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ExitResign extends Model
 {
-    //
+    use SoftDeletes;
+    
     public function Employee()
     {
         return $this->belongsTo(Employee::class);
